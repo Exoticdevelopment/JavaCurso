763b51c70a298849f81d6df4543251e4a0257950
Exoticdevelopment <exoticdev777@gmail.com>
2023-02-07 12:52:54 -0500

    Quinto

---
+++ src/Funciones/JavaMethods.java
@@ -0,0 +1,69 @@
+package Funciones;
+
+public class JavaMethods {
+
+    public static void main(String[] args) {
+
+        //OPENBOOTCAMP 1 CLASE: QUE ES UNA FUNCION:
+
+        System.out.println("Bienvenidos al E-commerce de zapatillas");
+        System.out.println("1- Ver zapatillas");
+        System.out.println("2- Comprar Zapatillas");
+        System.out.println("3- Salir");
+
+        /* Una funcion es un bloque de codigo que nos ayuda a no repetir codigo */
+
+
+        //W3 SCHOOLS
+
+        /* A method is a block of code which only runs when it is called.
+        You can pass data , known as parameters , into a method
+        Methods are used to perform (llevar a cabo) certain (cierta) actions , and they are also know
+        as fuctions.
+
+        Why use methods? To reuse code , define the code once , and use it many times.
+         */
+
+
+       }
+
+
+        //OPEN BOOTCAMP 2 CLASE: COMO SE CREA UNA FUNCION:
+
+       //La funcion debe estar por fuera del metodo main.
+
+        // void es una palabra reservada que indica que no devuelve nada la funcion
+        void showMenu(){
+
+            System.out.println("Bienvenidos al E-commerce de zapatillas");
+            System.out.println("1- Ver zapatillas");
+            System.out.println("2- Comprar Zapatillas");
+            System.out.println("3- Salir");
+
+        }
+
+        //W3SCHOOL CREATE A METHOD:
+
+    /* A method must be declared within a class.(Un método debe declararse dentro de una clase.). It is defined with the name of the method , followed by parentheses ().
+    Java provides(proporciona) some pre-defined methods , such as  (como) System.out.println() , but you can also create your own methods
+    to perform certain actions:
+
+
+    Example:
+
+    Create a method inside Main:
+
+    public class Main {
+      static void myMethod() {
+      //code to be executed
+      }
+    }
+
+    Example Explained:
+
+    -myMethod() is the name of the method
+    -static means (manera o medio) that the method belongs (pertenece) to the Main class and not an object of the Main class.
+    -void means that this method does not have a return value .
+     */
+
+}
